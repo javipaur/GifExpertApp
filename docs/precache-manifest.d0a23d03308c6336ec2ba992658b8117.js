9bac171b4932605bd4b12f527f3fac098b88a290
Javipaur <javipaur@gmail.com>
2020-09-03 12:23:31 +0200

    pereparando GithubPages

---
+++ docs/precache-manifest.d0a23d03308c6336ec2ba992658b8117.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59f7b4a67777fd7ae4675e41c4ed5bdb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7895bb20e723865d4325",
+    "url": "/static/css/2.e2dd9b0a.chunk.css"
+  },
+  {
+    "revision": "f3858adf2f658104b859",
+    "url": "/static/css/main.dc2a7d37.chunk.css"
+  },
+  {
+    "revision": "7895bb20e723865d4325",
+    "url": "/static/js/2.0cae06fc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0cae06fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3858adf2f658104b859",
+    "url": "/static/js/main.c9e1dadb.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
